d537dfef32c3c3bc4b4101cfcb86edec129b44f3
stay7 <codelatte20@gmail.com>
2023-03-19 23:10:52 +0900

    카테고리 탭에 카테고리를 노출한다

--- src/components/tabs/categoryTab.tsx
+++ src/components/tabs/categoryTab.tsx
@@ -1,10 +1,38 @@
 import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { accountBooksState, selectedAccountBookState } from "../../state/accountBookState";
+import { Category } from "../../domain/accountBook/category";
 
 export const CategoryTab = () => {
-  return <Container>여기는 카테고리</Container>;
+  const accountBook = useRecoilValue(selectedAccountBookState);
+
+  return (
+    <Container>
+      {accountBook?.categories.map((value) => (
+        <CategoryView category={value} />
+      ))}
+    </Container>
+  );
 };
 
 const Container = styled.div`
   flex: 3;
   background-color: #f1f1f1;
 `;
+
+interface ICategoryViewProp {
+  category: Category;
+}
+
+const CategoryView = ({ category }: ICategoryViewProp) => {
+  return (
+    <>
+      <div>{category.name}</div>
+      <div>
+        {category.subCategories?.map((value) => (
+          <div>{value.name}</div>
+        ))}
+      </div>
+    </>
+  );
+};
